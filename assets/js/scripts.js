6103da43d27cb05727bd1e70bf5395c93e39e2e3
NaeemaBsharat <naeemabsharat1@gmail.com>
2023-09-19 21:49:09 +0300

    Update js spaces

---
+++ assets/js/scripts.js
@@ -0,0 +1,27 @@
+
+// script.js
+
+const galleryItems = document.querySelectorAll('.gallery-item');
+const lightbox = document.getElementById('lightbox');
+const lightboxImage = document.getElementById('lightbox-image');
+const closeLightbox = document.querySelector('.close');
+
+galleryItems.forEach(item => {
+    item.addEventListener('click', e => {
+        e.preventDefault();
+        const imageUrl = item.getAttribute('href');
+        lightboxImage.setAttribute('src', imageUrl);
+        lightbox.style.display = 'block';
+    });
+   
+    
+    
+});
+
+closeLightbox.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
